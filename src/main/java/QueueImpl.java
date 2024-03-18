8a1cd9e68da3f5c58f4116cbbf8a3e40008de2e3
Antoni Oller Arcas <aollera@uoc.edu>
2024-03-18 10:33:13 +0100

    first commit

---
+++ src/main/java/QueueImpl.java
@@ -0,0 +1,29 @@
+public class QueueImpl<E> implements Queue<E>{
+    private E[] data;
+    private int p;
+
+    public QueueImpl(int len) {
+        // TO-DO
+    }
+
+    public void push(E e) throws FullQueueException {
+        // TO-DO
+    }
+
+
+    public E pop() throws EmptyQueueException {
+        // TO-DO
+    }
+
+    private boolean isFull() {
+        // TO-DO
+    }
+
+    private boolean isEmpty() {
+        // TO-DO
+    }
+
+    public int size() {
+        //TO-DO
+    }
+}
