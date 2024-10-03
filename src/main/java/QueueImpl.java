7a0161de738f4848c846bdf5fcdb49b0d84874c9
Antoni Oller Arcas <aollera@uoc.edu>
2024-10-03 11:13:33 +0200

    s'afegeixen return

--- src/main/java/QueueImpl.java
+++ src/main/java/QueueImpl.java
@@ -13,17 +13,21 @@ public class QueueImpl<E> implements Queue<E>{
 
     public E pop() throws EmptyQueueException {
         // TO-DO
+        return null;
     }
 
     private boolean isFull() {
         // TO-DO
+        return false;
     }
 
     private boolean isEmpty() {
         // TO-DO
+        return false;
     }
 
     public int size() {
         //TO-DO
+        return 0;
     }
 }
